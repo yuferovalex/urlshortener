ca79a2f9d5f1d134efdeaab61597b3dca1f21b77
Alex Yuferov <yuferov.alex@gmail.com>
2019-01-01 23:04:39 +0500

    Удален лишний код

--- src/main/java/com/example/demo/service/UrlServiceException.java
+++ src/main/java/com/example/demo/service/UrlServiceException.java
@@ -1,18 +1,11 @@
 package com.example.demo.service;
 
 public class UrlServiceException extends RuntimeException {
-    UrlServiceException() {
-    }
-
     UrlServiceException(String message) {
         super(message);
     }
 
     UrlServiceException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    UrlServiceException(Throwable cause) {
-        super(cause);
     }
 }
