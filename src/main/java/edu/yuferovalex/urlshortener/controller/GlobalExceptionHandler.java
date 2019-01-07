e7b7b0d871037ce2d869a87a916cab2a9f94c5aa
Aleksandr Yuferov <yuferov.alex@gmail.com>
2019-01-07 22:35:01 +0500

    Запись всех ошибок в лог

--- src/main/java/edu/yuferovalex/urlshortener/controller/GlobalExceptionHandler.java
+++ src/main/java/edu/yuferovalex/urlshortener/controller/GlobalExceptionHandler.java
@@ -1,24 +1,27 @@
 package edu.yuferovalex.urlshortener.controller;
 
 import lombok.Value;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ValidationException;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Value
-    public class ErrorResponse {
+    class ErrorResponse {
         private String timestamp;
         private String path;
         private String message;
@@ -26,16 +29,37 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ValidationException.class)
-    @ResponseStatus(BAD_REQUEST)
     @ResponseBody
-    public ErrorResponse processValidationError(HttpServletRequest request, Exception exception) {
-        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
-        return new ErrorResponse(
-                timestamp,
+    public ResponseEntity<ErrorResponse> processValidationError(HttpServletRequest request, Exception exception) {
+        return createErrorResponse(request, BAD_REQUEST, exception);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<ErrorResponse> defaultExceptionHandler(HttpServletRequest request, Exception exception) {
+        ResponseStatus annotation = AnnotationUtils.findAnnotation(exception.getClass(), ResponseStatus.class);
+        HttpStatus status = annotation == null
+                ? HttpStatus.INTERNAL_SERVER_ERROR
+                : annotation.value();
+        return createErrorResponse(request, status, exception);
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponse(
+            HttpServletRequest request,
+            HttpStatus status,
+            Exception exception
+    ) {
+        if (status.is5xxServerError()) {
+            logger.error("unhandled exception caught", exception);
+        } else {
+            logger.warn(exception.getClass().getName() + ": " + exception.getLocalizedMessage());
+        }
+        return ResponseEntity.status(status).body(new ErrorResponse(
+                LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
                 request.getRequestURL().toString(),
                 exception.getLocalizedMessage(),
-                BAD_REQUEST.value()
-        );
+                status.value()
+        ));
     }
 
 }
