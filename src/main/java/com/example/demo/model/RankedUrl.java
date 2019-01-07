9df729ee1a0a759bb1c81050731872077e57175f
Aleksandr Yuferov <yuferov.alex@gmail.com>
2019-01-07 19:05:29 +0500

    Имя поля redirects изменено на count в соответсвии с ТЗ

--- src/main/java/com/example/demo/model/RankedUrl.java
+++ src/main/java/com/example/demo/model/RankedUrl.java
@@ -3,12 +3,15 @@ package com.example.demo.model;
 import com.example.demo.controller.RedirectController;
 import com.example.demo.utils.Base62;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public interface RankedUrl {
     Integer getRank();
-    Integer getRedirects();
     String getOriginal();
+
+    @JsonProperty("count")
+    Integer getRedirects();
 
     @JsonIgnore
     Integer getId();
